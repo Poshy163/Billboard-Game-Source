ef8344632bc9ed9e82b17d459b9e6a50a1273e93
Joshua Leaper <poshernater@outlook.com>
2021-06-03 17:30:52 +0930

    Fixed packages error

--- Assets/Scripts/Other/GlobalVar.cs
+++ Assets/Scripts/Other/GlobalVar.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
-
-public class GlobalVar : MonoBehaviour
+public class GlobalVar:MonoBehaviour
 {
     public const int amountOfLevels = 4;
     public static string Name = null;
